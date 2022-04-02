328bba8f51707af38ecefcb8967a8c04e35f3564
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2022-04-02 13:35:26 -0500

    added resuable button

---
+++ src/components/UI/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './Card.module.css'
+
+const Card = props => {
+
+//addition of template literals in className passes CSS properties as props
+    return (
+        <div className={`${classes.card} ${props.className}`}>
+            {props.children}
+        </div>
+    )
+
+}
+
+export default Card;
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,22 +1,26 @@
 import React from 'react';
+import Card from '../UI/Card';
+import Button from '../UI/Button';
+import classes from './AddUser.module.css';
 
 const AddUser = props => {
-
 
     const addUserHandler = (event) => {
         event.preventDefault();
     }
 
     return (
-        <form onSubmit={addUserHandler}>
-            <label htmlFor='username'>Username</label>
-            <input id='username' type="text" />
+        <Card className={classes.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor='username'>Username</label>
+                <input id='username' type="text" />
 
-            <label htmlFor='age'>Age (Years)</label>
-            <input id='age' type="number" />
+                <label htmlFor='age'>Age (Years)</label>
+                <input id='age' type="number" />
 
-            <button type="submit">Add User</button>
-        </form>
+                <Button>Add User</Button>
+            </form>
+        </Card>
     )
 }
 
